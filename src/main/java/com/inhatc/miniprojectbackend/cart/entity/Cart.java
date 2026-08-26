21b2f350b4d05c381c5d5ed02788d0ede9e0cc8a
immin0241 <immin1029@outlook.com>
2026-08-26 13:09:42 +0900

    fix: DB 외래키 제약으로 장바구니가 삭제되지 않는 버그 수정

--- src/main/java/com/inhatc/miniprojectbackend/cart/entity/Cart.java
+++ src/main/java/com/inhatc/miniprojectbackend/cart/entity/Cart.java
@@ -1,6 +1,7 @@
 package com.inhatc.miniprojectbackend.cart.entity;
 
 import com.inhatc.miniprojectbackend.restaurant.entity.Restaurant;
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
@@ -9,8 +10,11 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -34,6 +38,9 @@ public class Cart {
     @ManyToOne(fetch = FetchType.LAZY, optional = false)
     @JoinColumn(name = "restaurant_id", nullable = false)
     private Restaurant restaurant;
+
+    @OneToMany(mappedBy = "cart", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<CartItem> cartItems = new ArrayList<>();
 
     @CreationTimestamp
     @Column(name = "created_at", nullable = false, updatable = false)
